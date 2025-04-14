8dfa80baf247c8b7dd864609bd15dd4b89531767
Rafinaldi <rafinaldideraizel@gmail.com>
2025-04-14 14:08:34 +0700

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,9 +51,13 @@ const Navbar = () => {
           </li>
           <li className="nav-item">
             <a href="#" className="nav-link text-white">
-              <i className="bi bi-gear"></i> Settings
             </a>
           </li>
+          <li className="nav-item">
+            <Link to="/About" className="nav-link text-white">
+              <i className="bi bi-person"></i> About
+            </Link>
+            </li>
         </ul>
       </div>
     </>
---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='p-4'>
+        <h4>Biodata diri</h4>
+        <p>Nama : RAFINALDI</p>
+        <p>NIM : 12350112955</p>
+        <p>Prodi : Teknik Informatika</p>
+        <p>Hobi: Sepak Bola</p>
+    </div>
+  );
+};
+
+export default About
